1219a25d63432c40452acbdfcc1622b0a001154e
Student0544 <78926696+Student0544@users.noreply.github.com>
2023-03-09 01:02:06 -0500

    Update find_color_coordinates.py

--- find_color_coordinates.py
+++ find_color_coordinates.py
@@ -4,12 +4,12 @@ import numpy as np
 import csv
 
 
-def find_coordinates(folder_path, *target_colors):
+def find_coordinates(folder_path: str, margin: int, *target_colors: list):
 
     """Returns a CSV file containing the coordinates corresponding to the colors' position. Each row corresponds to
     the position of one individual object, and a blank row means that it's the coordinates of the next image.
     If multiple target colors are entered, then the contours will treat those overlapping colors as one
-    object. Please enter BGR values for the colors"""
+    object. Please enter BGR values for the colors. Margin is the variation in BGR value allowed"""
 
     errors = 0
 
@@ -30,7 +30,7 @@ def find_coordinates(folder_path, *target_colors):
             image = cv2.imread(filepath)
 
             # Turn the Image into a Binary Image Based on the Target Colors
-            masks = [cv2.inRange(image, array-10, array+10) for array in colors]
+            masks = [cv2.inRange(image, array-margin, array+margin) for array in colors]
 
             if len(masks) > 1:
                 for index in range(len(masks)):
@@ -43,7 +43,11 @@ def find_coordinates(folder_path, *target_colors):
             else:
                 mask = masks[0]
 
+            # Turn the Image into a Binary Image. Also smooths out noises and small spots.
             binary_image = cv2.threshold(mask, 127, 255, cv2.THRESH_BINARY)[1]
+            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+            binary_image = cv2.morphologyEx(binary_image, cv2.MORPH_CLOSE, kernel)
+            binary_image = cv2.morphologyEx(binary_image, cv2.MORPH_OPEN, kernel)
 
             contours = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
             ind_contours = [np.array(contour) for contour in contours]
@@ -66,27 +70,41 @@ def find_coordinates(folder_path, *target_colors):
                     obj.writerow(row)
                 obj.writerow([])
 
-            if img_name == os.listdir(input_folder)[0]:
-                copy = image.copy()
+            if img_name in os.listdir(input_folder)[0:4]:
+                
+                copy1 = image.copy()
+                copy2 = image.copy()
+
                 for coordinate in coordinates:
+
                     # cv2 takes ranges of pixels in the format of [yi:yf, xi:xf]
-                    region = copy[coordinate[2]:coordinate[3], coordinate[0]:coordinate[1]]
+                    region = copy1[coordinate[2]:coordinate[3], coordinate[0]:coordinate[1]]
 
                     blur = cv2.GaussianBlur(region, (31, 31), 0, 0)
-                    copy[coordinate[2]:coordinate[3], coordinate[0]:coordinate[1]] = blur
+                    copy1[coordinate[2]:coordinate[3], coordinate[0]:coordinate[1]] = blur
+                    copy2[coordinate[2]:coordinate[3], coordinate[0]:coordinate[1]] = [255, 255, 255]
 
-                edit = cv2.hconcat([image, copy])
+                edit = cv2.hconcat([image, copy1, copy2])
 
-                if edit.shape[1] > 1000:
-                    new_height = int(edit.shape[0]/edit.shape[1] * 1000)
-                    edit = cv2.resize(edit, (new_height, 1000))
+                if edit.shape[1] > 1500:
+                    new_height = int(edit.shape[0]/edit.shape[1] * 1500)
+                    edit = cv2.resize(edit, (new_height, 1500))
+
                 cv2.imshow("First Edit", edit)
                 cv2.waitKey(0)
                 cv2.destroyAllWindows()
 
-        except TypeError:
+        except ValueError:
             errors += 1
 
     plural_noun = {True: "", False: "s"}[errors < 1]
     plural_verb = {True: "was", False: "were"}[errors < 1]
-    print(f"Done. {errors} image{plural_noun} {plural_verb} unsuccessfully processed.")
+    print(f"{len(os.listdir(input_folder)) - errors} image{plural_noun} processed. "
+          f"{errors} image{plural_noun} {plural_verb} unsuccessfully processed.")
+
+
+face_color = [196, 196, 196]
+hat_color = [16, 16, 16]
+hair_color = [31, 31, 31]
+find_coordinates(##############################################, 5,
+                 face_color, hat_color, hair_color)
